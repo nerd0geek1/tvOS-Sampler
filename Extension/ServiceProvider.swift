c2f4f4cacc1217a3f6a3417745e6b0e29f5260fe
Kohei Tabata <nerd0geek1@gmail.com>
2016-01-25 07:42:18 +0900

    add TV Services Extension target

---
+++ Extension/ServiceProvider.swift
@@ -0,0 +1,31 @@
+//
+//  ServiceProvider.swift
+//  Extension
+//
+//  Created by Kohei Tabata on 2016/01/25.
+//  Copyright © 2016年 Kohei Tabata. All rights reserved.
+//
+
+import Foundation
+import TVServices
+
+class ServiceProvider: NSObject, TVTopShelfProvider {
+
+    override init() {
+        super.init()
+    }
+
+    // MARK: - TVTopShelfProvider protocol
+
+    var topShelfStyle: TVTopShelfContentStyle {
+        // Return desired Top Shelf style.
+        return .Sectioned
+    }
+
+    var topShelfItems: [TVContentItem] {
+        // Create an array of TVContentItems.
+        return []
+    }
+
+}
+
